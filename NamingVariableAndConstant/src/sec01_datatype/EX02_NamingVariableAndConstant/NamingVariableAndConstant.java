5451609eee9e3c704dd3ba291821ebd400113bfe
HEA <hea_s@naver.com>
2022-09-20 14:39:26 +0900

    Initial commit

---
+++ NamingVariableAndConstant/src/sec01_datatype/EX02_NamingVariableAndConstant/NamingVariableAndConstant.java
@@ -0,0 +1,23 @@
+package sec01_datatype.EX02_NamingVariableAndConstant;
+
+public class NamingVariableAndConstant {
+	public static void main(String[] args) {
+		//
+		boolean aBcD;
+		byte °¡³ª´Ù;
+		short _abcd;
+		char $ab_cd;
+		//
+		long abcd3;
+		//
+		double main;
+		//
+		string myClassName;
+		int ABC;
+		
+		//
+		final double PI;
+		final int MT_DATA;
+		final float myData;
+	}
+}
